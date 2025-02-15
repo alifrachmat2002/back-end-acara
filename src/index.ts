8bb5cc3e20936e3cf5dc0224b37feffca4dda175
alifrachmat2002 <aaliplagi@gmail.com>
2025-02-15 11:54:56 +0700

    feat: Initialize server with database connection and body-parser middleware
    changes
    - Defined DB connection in `database.ts`
    - Implemented DB connection in `index.ts`
    - Added env.ts to load environment variables with dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,39 @@
 import express, { Request, Response } from "express";
 import router from "./routes/api";
-const app = express();
+import bodyParser from "body-parser";
+import db from "./utils/database";
 
-const PORT = 3000;
+// Init function to start the server
+async function init() {
+    try {
+        // Connect to the database
+        const result = await db();
+        console.log(`Database Status : ${result}`)
 
-app.use("/api", router);
-app.get('/',(req : Request, res: Response) => {
-    res.status(200).json({
-        messsage : "Success",
-        data : "Welcome to the dummy app"
-    });
-})
+        // Create an express app
+        const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+        // Define the port
+        const PORT = 3000;
+
+        // Use the body parser middleware
+        app.use(bodyParser.json());
+        // Use the router
+        app.use("/api", router);
+        // Define the default route
+        app.get("/", (req: Request, res: Response) => {
+            res.status(200).json({
+                messsage: "Success",
+                data: "Welcome to the dummy app",
+            });
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+init()
