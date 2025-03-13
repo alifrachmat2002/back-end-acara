71b2d280c4bd5482d215cbdce35dee6df6381e27
alifrachmat2002 <aaliplagi@gmail.com>
2025-03-13 15:00:56 +0700

    feat: Implement access control middleware and update user roles
    changes
    - added list of user roles in `/utils/constant.ts`
    - added access control middleware in `acl.middleware.ts`
    - updated user roles definition in `user.model.ts`
    - added test route for access control middleware in `/routes/api.ts`

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { encrypt } from "../utils/encryption";
 import { renderHtmlContent, sendMail } from "../utils/mail/mail";
 import { CLIENT_HOST, EMAIL_SMTP_USER } from "../utils/env";
+import { ROLES } from "../utils/constant";
 
 const Schema = mongoose.Schema;
 
@@ -40,8 +41,8 @@ const UserSchema = new Schema<User>({
     },
     role : {
         type : Schema.Types.String,
-        enum : ['user', 'admin'],
-        default : 'user'
+        enum : [ROLES.ADMIN, ROLES.MEMBER],
+        default : ROLES.MEMBER
     },
     profilePicture : {
         type : Schema.Types.String,
