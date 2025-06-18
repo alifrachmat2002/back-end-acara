d7c5c9361bf03e09207f5694e7cbd558a9f095d3
alifrachmat2002 <aaliplagi@gmail.com>
2025-06-18 16:37:24 +0700

    feat: enhance order pagination, order retrieval, and order completion logic

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,9 +1,12 @@
 import { Response } from "express";
 import { IReqUser } from "../utils/interfaces";
 import response from "../utils/response";
-import OrderModel, { orderDAO, TypeOrder } from "../models/order.model";
+import OrderModel, { orderDAO, OrderStatus, TypeOrder, TypeVoucher } from "../models/order.model";
 import TicketModel from "../models/ticket.model";
 import EventModel from "../models/event.model";
+import { FilterQuery } from "mongoose";
+import { resolveProjectReferencePath } from "typescript";
+import { getId } from "../utils/id";
 
 export default {
     async create(req: IReqUser, res: Response) {
@@ -38,13 +41,67 @@ export default {
         }
     },
     async findAll(req: IReqUser, res: Response) {
-        try {
-            
+        try {          
+            const buildQuery = (filter: any) => {
+                let query: FilterQuery<TypeOrder> = {};
+
+                if (filter.search) query.$text = { $search: filter.search };
+
+                return query;
+            };
+
+            const {
+                limit = 10,
+                page = 1,
+                search,               
+            } = req.query;
+
+            const query = buildQuery({
+                limit,
+                page,
+                search
+            });
+
+            const result = await OrderModel.find(query)
+                .skip((+page - 1) * +limit)
+                .limit(+limit)
+                .sort({ createdAt: -1 })
+                .lean()
+                .exec();
+
+            const count = await OrderModel.countDocuments(query);
+
+            return response.pagination(
+                res,
+                result,
+                "Order data fetched successfully!",
+                {
+                    currentPage: +page,
+                    total: count,
+                    totalPages: Math.ceil(count / +limit),
+                }
+            );
         } catch (error) {
-            return response.error(res, error,"Failed")
+            return response.error(res, error, "Failed fetching order data.");
         }
     },
     async findOne(req: IReqUser, res: Response) {
+        try {
+            const { orderId } = req.params;
+            const result = await OrderModel.findOne({
+                orderId
+            });
+
+            if (!result) {
+                return response.notFound(res, "Order is not found.");
+            }
+
+            return response.success(res, result, "Order data fetched successfully!");
+        } catch (error) {
+            return response.error(res, error, "Failed fetching order data.");
+        }
+    },
+    async findAllByMember(req: IReqUser, res: Response) {
         try {
             
         } catch (error) {
@@ -52,11 +109,65 @@ export default {
         }
     },
     
-    async completed(req: IReqUser, res: Response) {
+    async complete(req: IReqUser, res: Response) {
         try {
-            
+            const { orderId } = req.params;
+            const user = req.user?.id;
+
+            const order = await OrderModel.findOne({
+                orderId,
+                createdBy: user,
+            });
+
+            if (!order) {
+                return response.notFound(res, "Order is not found.");
+            }
+
+            if (order.status === OrderStatus.COMPLETED) {
+                return response.error(res, null, "Order has been completed.");
+            }
+
+            const ticket = await TicketModel.findById(`${order?.ticket}`);
+
+            if (!ticket) {
+                return response.notFound(res, "Ticket is not found.");
+            }
+
+            const vouchers: TypeVoucher[] = Array.from(
+                { length: order.quantity },
+                () => {
+                    return {
+                        isPrinted: false,
+                        voucherId: getId(),
+                    };
+                }
+            );
+
+            const result = await OrderModel.findOneAndUpdate(
+                {
+                    orderId,
+                    createdBy: user,
+                },
+                {
+                    status: OrderStatus.COMPLETED,
+                    vouchers,
+                },
+                {
+                    new: true,
+                }
+            );
+
+            await TicketModel.findByIdAndUpdate(ticket.id, {
+                quantity: ticket.quantity - order.quantity,
+            });
+
+            return response.success(
+                res,
+                result,
+                "Order completed successfully."
+            );
         } catch (error) {
-            return response.error(res, error,"Failed")
+            return response.error(res, error,"Failed to complete order.");
         }
     },
     async pending(req: IReqUser, res: Response) {
